fe844770f3724bf2db6d5adeb1eb805219316754
Rihab Zouitni <zouitnirihab@gmail.com>
2023-02-06 19:41:50 +0100

    test

---
+++ pages/Welcome.js
@@ -0,0 +1,11 @@
+const Welcome = () => {
+    return (
+        <>
+        <h1> Welcome </h1>
+        <p> this is (for sure !) my first next web </p>
+        <p> testing out </p>
+        </>
+    )
+}
+
+export default Welcome;
